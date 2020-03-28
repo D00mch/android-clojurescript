df0dd20144a94a540b38c46ff2f7533e1bcc3b33
18385139 <arturdumchev@yandex.ru>
2020-03-29 01:08:58 +0300

    rm generated

--- cljsrc/resources/public/cljs-out/dev/figwheel/main/generated/dev_auto_test_runner.cljs
+++
@@ -1,5 +0,0 @@
-(ns figwheel.main.generated.dev-auto-test-runner
-  (:require [cljs.test :refer-macros [run-tests]]
-            [cljs-test-display.core] [cljsrc.core-test]))
-
-  (run-tests (cljs-test-display.core/init! "app-auto-testing") (quote cljsrc.core-test))
